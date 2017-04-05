c85ae5b7e8889110b817c5bc721a10c4d24b36df
jensmeder <kontakt@jensmeder.de>
2017-04-05 11:42:28 +0200

    Add iOS sample

---
+++ Messenger-iOS/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Messenger-iOS
+//
+//  Created by Jens Meder on 05.04.17.
+//
+//
+
+import UIKit
+import DarkLightning
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    var port: DarkLightning.Port?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        port = DevicePort(delegate: DevicePortDelegateFake(), dataDelegate: DataDecodingFake())
+        port?.open()
+        return true
+    }
+
+
+}
+
