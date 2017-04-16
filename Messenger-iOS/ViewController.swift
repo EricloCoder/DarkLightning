004cc1a963a207ef833617c24308f5dec0886afe
jensmeder <kontakt@jensmeder.de>
2017-04-16 21:39:00 +0200

    Add styling for iOS UI

--- Messenger-iOS/ViewController.swift
+++ Messenger-iOS/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     private var button: UIButton?
     private let header: Memory<UINavigationItem?>
     private let port: DarkLightning.Port
+    private var bottomConstraint: NSLayoutConstraint?
+    private var recognizer: UITapGestureRecognizer?
 
     init(title: String, textView: Memory<UITextView?>, header: Memory<UINavigationItem?>, port: DarkLightning.Port) {
         self.textView = textView
@@ -30,14 +32,31 @@ class ViewController: UIViewController {
     }
 
     override func loadView() {
+        self.recognizer = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
         let container = UIView()
+        let textFieldContainer = UIView()
+        let separator = UIView()
+        separator.backgroundColor = UIColor(white: 0.87, alpha: 1.0)
+        separator.translatesAutoresizingMaskIntoConstraints = false
+        textFieldContainer.translatesAutoresizingMaskIntoConstraints = false
+        textFieldContainer.backgroundColor = UIColor.white
+        textFieldContainer.layer.cornerRadius = 13.0
+        textFieldContainer.layer.borderWidth = 1.0
+        textFieldContainer.layer.borderColor = UIColor(white: 0.87, alpha: 1.0).cgColor
+        let messageContainer = UIView()
+        messageContainer.translatesAutoresizingMaskIntoConstraints = false
+        messageContainer.backgroundColor = UIColor(white: 0.97, alpha: 1.0)
         button = UIButton()
+        button?.titleLabel?.font = UIFont.systemFont(ofSize: 16.0)
+        button?.setTitleColor(UIColor.blue, for: .normal)
+        button?.setTitleColor(UIColor.lightGray, for: .disabled)
         button?.addTarget(self, action: #selector(send(button:)), for: .touchUpInside)
         button?.setTitle("Send", for: .normal)
         textField = UITextField()
+        textField?.font = UIFont.systemFont(ofSize: 14.0)
         textField?.addTarget(self, action: #selector(textDidChange(textField:)), for: .editingChanged)
-        textField?.backgroundColor = UIColor.white
         textView.rawValue = UITextView()
+        textView.rawValue?.addGestureRecognizer(recognizer!)
         textView.rawValue?.isEditable = false
         textView.rawValue?.translatesAutoresizingMaskIntoConstraints = false
         textField?.translatesAutoresizingMaskIntoConstraints = false
@@ -45,19 +64,36 @@ class ViewController: UIViewController {
         button?.setContentHuggingPriority(UILayoutPriorityRequired, for: .horizontal)
         textField?.setContentHuggingPriority(UILayoutPriorityDefaultLow, for: .horizontal)
         container.addSubview(textView.rawValue!)
-        container.addSubview(textField!)
-        container.addSubview(button!)
+        textFieldContainer.addSubview(textField!)
+        messageContainer.addSubview(textFieldContainer)
+        messageContainer.addSubview(button!)
+        container.addSubview(messageContainer)
+        container.addSubview(separator)
+        bottomConstraint = messageContainer.bottomAnchor.constraint(equalTo: container.bottomAnchor)
         let constraints = [
+            textField?.leftAnchor.constraint(equalTo: textFieldContainer.leftAnchor, constant: 10),
+            textField?.rightAnchor.constraint(equalTo: textFieldContainer.rightAnchor, constant: -10),
+            textField?.topAnchor.constraint(equalTo: textFieldContainer.topAnchor, constant: 5),
+            textField?.bottomAnchor.constraint(equalTo: textFieldContainer.bottomAnchor, constant: -5),
             textView.rawValue?.topAnchor.constraint(equalTo: container.topAnchor),
-            textView.rawValue?.bottomAnchor.constraint(equalTo: textField!.topAnchor),
+            textView.rawValue?.bottomAnchor.constraint(equalTo: separator.topAnchor, constant: 1.0),
             textView.rawValue?.leftAnchor.constraint(equalTo: container.leftAnchor),
             textView.rawValue?.rightAnchor.constraint(equalTo: container.rightAnchor),
-            textField?.leftAnchor.constraint(equalTo: container.leftAnchor),
-            textField?.bottomAnchor.constraint(equalTo: container.bottomAnchor),
-            button!.leftAnchor.constraint(equalTo: textField!.rightAnchor),
-            button!.rightAnchor.constraint(equalTo: container.rightAnchor),
-            button!.bottomAnchor.constraint(equalTo: container.bottomAnchor),
-            button!.heightAnchor.constraint(equalTo: textField!.heightAnchor)
+            textFieldContainer.leftAnchor.constraint(equalTo: messageContainer.leftAnchor, constant: 5),
+            textFieldContainer.topAnchor.constraint(equalTo: messageContainer.topAnchor, constant: 5),
+            textFieldContainer.bottomAnchor.constraint(equalTo: messageContainer.bottomAnchor, constant: -5),
+            button!.leftAnchor.constraint(equalTo: textFieldContainer.rightAnchor, constant: 10),
+            button!.rightAnchor.constraint(equalTo: messageContainer.rightAnchor, constant: -10),
+            button?.bottomAnchor.constraint(equalTo: messageContainer.bottomAnchor),
+            button!.topAnchor.constraint(equalTo: messageContainer.topAnchor),
+            messageContainer.rightAnchor.constraint(equalTo: container.rightAnchor),
+            bottomConstraint,
+            messageContainer.leftAnchor.constraint(equalTo: container.leftAnchor),
+            separator.heightAnchor.constraint(equalToConstant: 1.0),
+            separator.leftAnchor.constraint(equalTo: container.leftAnchor),
+            separator.rightAnchor.constraint(equalTo: container.rightAnchor),
+            separator.bottomAnchor.constraint(equalTo: messageContainer.topAnchor)
+            
         ]
         for constraint in constraints {
             constraint?.isActive = true
@@ -69,11 +105,15 @@ class ViewController: UIViewController {
         super.viewWillAppear(animated)
         self.header.rawValue = navigationItem
         button?.isEnabled = false
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         self.header.rawValue = nil
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
     }
     
     // MARK: Text Field Events
@@ -91,6 +131,34 @@ class ViewController: UIViewController {
         textField?.text = nil
         button.isEnabled = false
         port.write(data: textField!.text!.data(using: .utf8)!)
+    }
+    
+    // MARK: Keyboard Events
+    
+    @objc private func keyboardWillShow(notification: Notification) {
+        let info = notification.userInfo!
+        let keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        let keyboardHeight: CGFloat = keyboardSize.height
+        let duration = info[UIKeyboardAnimationDurationUserInfoKey] as! TimeInterval
+        UIView.animate(withDuration: duration, delay: 0, options: UIViewAnimationOptions.curveEaseInOut, animations: {
+            self.bottomConstraint?.constant = -keyboardHeight
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    @objc private func keyboardWillHide(notification: Notification) {
+        let info = notification.userInfo!
+        let duration = info[UIKeyboardAnimationDurationUserInfoKey] as! TimeInterval
+        UIView.animate(withDuration: duration, delay: 0, options: UIViewAnimationOptions.curveEaseInOut, animations: {
+            self.bottomConstraint?.constant = 0
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    // MARK: Gesture Recognizer Events
+    
+    @objc private func closeKeyboard() {
+        textField?.resignFirstResponder()
     }
 }
 
