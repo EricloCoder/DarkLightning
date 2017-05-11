080cf9546ba6ebca594af173aed9ee201bbba1df
Jens Meder <kontakt@jensmeder.de>
2017-05-11 21:59:49 +0200

    Simplify Memory class

--- Sources/Utils/Sources/Memory.swift
+++ Sources/Utils/Sources/Memory.swift
@@ -29,34 +29,14 @@
 import Foundation
 
 public final class Memory<T> {
-    private let buffer: UnsafeMutablePointer<T>
-    
+
 	// MARK: Init
     
-    public convenience init(initialValue: T) {
-        let buffer = UnsafeMutablePointer<T>.allocate(capacity: 1)
-        buffer.initialize(to: initialValue)
-        self.init(buffer: buffer)
+    public required init(initialValue: T) {
+        self.rawValue = initialValue
     }
-    
-    public required init(buffer: UnsafeMutablePointer<T>) {
-        self.buffer = buffer
-    }
-    
-    // MARK: Private
-    
-    deinit {
-        buffer.deallocate(capacity: 1)
-    }
-    
+	
     // MARK: Public
     
-    public var rawValue: T {
-        get {
-            return buffer.pointee
-        }
-        set {
-            buffer.pointee = newValue
-        }
-    }
+    public var rawValue: T
 }
